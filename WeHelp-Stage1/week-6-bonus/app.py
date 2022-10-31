0cb2b0117f8695eaa8740d4a1d537204121d532c
Jamie Yu <43780809+jamieyu0914@users.noreply.github.com>
2022-10-31 20:22:05 +0800

    Update app.py

--- WeHelp-Stage1/week-6-bonus/app.py
+++ WeHelp-Stage1/week-6-bonus/app.py
@@ -3,7 +3,6 @@ from flask import request
 from flask import redirect 
 from flask import render_template 
 from flask import session
-from mysql.connector import Error 
 from mysql.connector import pooling
 
 
